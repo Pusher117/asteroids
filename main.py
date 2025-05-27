67e4b7eb405938be2540d6a0914ff1f4e8897c3b
Pusher117 <brm.mig@gmail.com>
2025-05-26 20:00:34 -0400

    canRotate,60fps lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main(): 
     pygame.init()
@@ -10,13 +11,21 @@ def main():
 ##Clock object
     clock=pygame.time.Clock()
     dt=0
+
+    player=Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)
+
+
     while True: 
         screen.fill((0,0,0))
+        
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-    dt=clock.tick(60) / 1000
+        dt=clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
